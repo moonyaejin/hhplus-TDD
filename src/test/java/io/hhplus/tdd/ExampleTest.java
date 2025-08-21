fef5a13ded2e10724fca0c4d2450fd6bbbae0dbe
moonyaejin <i0209i80@gmail.com>
2025-08-21 10:00:36 +0900

    test: 기본 테스트 디렉토리 및 샘플 테스트 추가

---
+++ src/test/java/io/hhplus/tdd/ExampleTest.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExampleTest {
+
+    @Test
+    void contextLoads() {
+        assertThat(1).isEqualTo(1);
+    }
+}
